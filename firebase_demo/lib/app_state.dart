59359d0efa9a91d5a2809f910371a0effd930f8e
elliotallen1 <allened@hendrix.edu>
2024-10-09 13:42:57 -0500

    Recording RSVP status
    Yes and NO added that will Update how many people have RSVPed

--- firebase_demo/lib/app_state.dart
+++ firebase_demo/lib/app_state.dart
@@ -10,11 +10,29 @@ import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'guest_book_message.dart';                        // new
 
+enum Attending { yes, no, unknown }
 
 class ApplicationState extends ChangeNotifier {
   ApplicationState() {
     init();
   }
+
+  int _attendees = 0;
+int get attendees => _attendees;
+
+Attending _attending = Attending.unknown;
+StreamSubscription<DocumentSnapshot>? _attendingSubscription;
+Attending get attending => _attending;
+set attending(Attending attending) {
+  final userDoc = FirebaseFirestore.instance
+      .collection('attendees')
+      .doc(FirebaseAuth.instance.currentUser!.uid);
+  if (attending == Attending.yes) {
+    userDoc.set(<String, dynamic>{'attending': true});
+  } else {
+    userDoc.set(<String, dynamic>{'attending': false});
+  }
+}
 
   bool _loggedIn = false;
   bool get loggedIn => _loggedIn;
@@ -30,6 +48,15 @@ class ApplicationState extends ChangeNotifier {
     FirebaseUIAuth.configureProviders([
       EmailAuthProvider(),
     ]);
+
+    FirebaseFirestore.instance
+        .collection('attendees')
+        .where('attending', isEqualTo: true)
+        .snapshots()
+        .listen((snapshot) {
+      _attendees = snapshot.docs.length;
+      notifyListeners();
+    });
     
     FirebaseAuth.instance.userChanges().listen((user) {
       if (user != null) {
@@ -47,6 +74,22 @@ class ApplicationState extends ChangeNotifier {
                 message: document.data()['text'] as String,
               ),
             );
+          }
+          notifyListeners();
+        });
+        _attendingSubscription = FirebaseFirestore.instance
+            .collection('attendees')
+            .doc(user.uid)
+            .snapshots()
+            .listen((snapshot) {
+          if (snapshot.data() != null) {
+            if (snapshot.data()!['attending'] as bool) {
+              _attending = Attending.yes;
+            } else {
+              _attending = Attending.no;
+            }
+          } else {
+            _attending = Attending.unknown;
           }
           notifyListeners();
         });
