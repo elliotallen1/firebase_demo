a21552705889d5713781f9eb21ebc42969f5064f
elliotallen1 <allened@hendrix.edu>
2024-12-15 19:25:41 -0600

    Added color to guest book

--- firebase_demo/lib/app_state.dart
+++ firebase_demo/lib/app_state.dart
@@ -72,6 +72,7 @@ set attending(Attending attending) {
               GuestBookMessage(
                 name: document.data()['name'] as String,
                 message: document.data()['text'] as String,
+                color: document.data()['color'] as Color
               ),
             );
           }
--- firebase_demo/lib/guest_book_message.dart
+++ firebase_demo/lib/guest_book_message.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 class GuestBookMessage {
-  GuestBookMessage({required this.name, required this.message});
+  GuestBookMessage({required this.name, required this.message, required this.color});
 
   final String name;
   final String message;
+  final Color color;
 }
